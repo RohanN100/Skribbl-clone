4b41315d6c7517444a480c320636916789417efb
RohanN100 <gorerohan37@gmail.com>
2026-09-15 19:56:07 +0530

    Fix Render server binding

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,6 @@ import { Server } from "socket.io";
 import app from "./app.js";
 import { registerSocketHandlers } from "./socket/socketHandler.js";
 
-
 const PORT = process.env.PORT || 5000;
 
 const httpServer = createServer(app);
@@ -16,6 +15,6 @@ const io = new Server(httpServer, {
 
 registerSocketHandlers(io);
 
-httpServer.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+httpServer.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
